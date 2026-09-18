196c16be1aaaa9245d6facc577385d31a029f6f2
Nitin Dwivedi <nitin.dwivedi@iphtechnologies.com>
2026-09-18 15:47:22 +0530

    Initial Commit

---
+++ VenzerFinance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VenzerFinance
+//
+//  Created by iPHTech 40 on 18/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
