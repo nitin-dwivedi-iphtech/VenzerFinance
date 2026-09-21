19a3e897e558123937ae47e2d884cc215cfdbc02
Nitin Dwivedi <nitin.dwivedi@iphtechnologies.com>
2026-09-21 11:14:16 +0530

    Revert "Revert "Feature/welcome screen""

---
+++ VenzerFinance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VenzerFinance
+//
+//  Created by iPHTech 40 on 18/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            WelcomeView()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
